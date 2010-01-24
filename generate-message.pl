a23a3364babb73a8129e4bb2f791d069929c022a
Jorj Bauer <jorj@jorj.org>
2010-01-24 00:20:02 -0500

    made message longer and clearer. added ">R" bulk-read to message.

--- generate-message.pl
+++ generate-message.pl
@@ -2,14 +2,18 @@
 
 my $msg = qq{
 
-SerEE v1.1
-Cmds:
->rXX  read 1 byte @ address XX (2 bytes, ltl-endian)
-      retns r1Y on success (Y is binary result byte)
+SerEE v1.2
+Commands:
+>rXX  read 1 byte @ address XX (2 bytes, little-endian)
+      returns r1Y on success (Y is binary result byte)
+>RXX  read XX (2 bytes, little-endian) bytes, starting @ addr 0
+      returns a dump of all bytes};
+
+my $msg2 = qq{
 >wXXY write value Y @ address XX
-      retns w1 on success
+      returns w1 on success
 >t    DESTRUCTIVE self-test
-      emits 'OK' on success
+      emits 'OK' on success or 'FAIL' on failure
 
 };
 
@@ -23,8 +27,26 @@ if (length($msg)+1 >= 255-$numrets) {
     my $len = length($msg)+1+$numrets;
     die "Message too long ($len)\n";
 }
+my $numrets2 = 0;
+foreach my $i (split(//, $msg2)) {
+    $numrets2++ 
+	if ($i =~ /^$/);
+}
+if (length($msg2)+1 >= 255-$numrets2) {
+}
 
-print "welcome_msg:\n\taddwf\tPCL, F\n";
+print "\torg\t0x40\nwelcome_msg2:\n\taddwf\tPCL, F\n";
+foreach my $i (split(//, $msg2)) {
+    if ($i =~ /^$/) {
+	print "\tretlw\t0x0A\n";
+	print "\tretlw\t0x0D\n";
+    } else {
+	print "\tretlw\t'$i'\n";
+    }
+}
+print "\tretlw\t0\n\n";
+
+print "\torg\t0x100\nwelcome_msg:\n\taddwf\tPCL, F\n";
 
 foreach my $i (split(//, $msg)) {
     if ($i =~ /^$/) {
@@ -35,3 +57,4 @@ foreach my $i (split(//, $msg)) {
     }
 }
 print "\tretlw\t0\n";
+
