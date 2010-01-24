53566072e2ebe213a681cbc9974723fb8e3d15d6
Jorj Bauer <jorj@jorj.org>
2010-01-24 16:35:29 -0500

    serial cleanup - added common functions from other projects, removed non-functional bitbanging code

--- serial.asm
+++ serial.asm
@@ -2,53 +2,58 @@
 
         include         "common.inc"
 	include		"globals.inc"
-	include		"delay.inc"
-
-;;; Define ECHO if you want characters to software-loopback to the sender
-;;; after being received. Good for testing.
-	
-#define ECHO 0
 
 ;;; ************************************************************************
 ;;; *
-;;; * Serial code, version 0.2: 3/2/2009
+;;; * Serial code, version 0.3: 1/24/2010
 ;;; *
-;;; * The bit-bang code here is untested. Notes from the pic-car system,
-;;; * from which I lifted this module (and improved it), say that the
-;;; * bit-bang interface had problems. YMMV.
-;;; *
-;;; * This code assumes that the big-bang data is buffered and inverted.
-;;; * 
 ;;; * This code assumes that the serial data is being buffered and inverted.
 ;;; * If the serial data is directly connected to an RS232 line (unbuffered),
 ;;; * then the sense of the RX and TX bits will be reverse (and the code
 ;;; * will need to be changed).
 ;;; *
-;;; * This code also assumes that you're running a 10 MHz clock. If the
-;;; * clock speed is different, you'll need to twiddle the magic constants
-;;; * (0xAA) to delay the right amount of time between bits.
+;;; * EXTERNAL REQUIREMENTS:
 ;;; *
-;;; * Lastly, the bit-banging code is 4800 Baud, 8/N/1. The USART version is
-;;; * probably different; you'll have to look at init_serial.
+;;; * These symbols need to be defined.
+;;; *   #define USART_HIGHSPEED <0|1>
+;;; *   #define USART_BAUD_INITIALIZER <constant>
+;;; *   #define USART_TX_TRIS PORTB, 2
+;;; *	#define USART_RX_TRIS PORTB, 1
+;;; *   #define USART_ECHO <0|1>
+;;; *
+;;; * The symbols USART_HIGHSPEED and USART_BAUD_INITIALIZER need to be
+;;; * calculated per the PIC's documentation. For the 16f627 series,
+;;; * here are the calculations:
+;;; *
+;;; *  high-speed: <constant> = ( (clock_speed_in_hertz) / (16 * baud rate) ) - 1
+;;; *   example: 10MHz, 4800 baud is:
+;;; *            (10000000 / (16 * 4800)) - 1 = 129.2 (use 129)
+;;; *  low-speed:  <constant> = ( (clock_speed_in_hertz) / (64 * baud rate) ) - 1
+;;; *   example: 10MHz, 4800 baud is:
+;;; *            (10000000 / (64 * 4800)) - 1 = 31.5 (try 31 or 32)
+;;; *
+;;; * The symbols USART_TX_TRIS and USART_RX_TRIS are specific to the PIC
+;;; * you're targeting. The TX and RX pins specified above are for the
+;;; * 16f627 and family.
+;;; *
+;;; Define USART_ECHO as 1 if you want characters to software-loopback to the 
+;;; sender immediately after being received. Good for testing.
 ;;; ************************************************************************
 
 	GLOBAL	init_serial
 	GLOBAL	putch_usart
+	GLOBAL	putch_hex_usart
 	GLOBAL	getch_usart
-#if BITBANG_ENABLED
-	GLOBAL	getch
-	GLOBAL	getch_timeout
-#endif
+	GLOBAL	getch_usart_timeout
 	
 ;;; ************************************************************************
 	udata
-loopcounter	res	1
-byte		res	1
-i		res	1
+serial_work_tmp	res	1
+serial_timeout_0	res	1
+serial_timeout_1	res	1
+serial_timeout_2	res	1
 
-timeout		res	3
-
-#if ECHO
+#if USART_ECHO
 echo_buf	res	1
 #endif
 	
@@ -56,51 +61,20 @@ echo_buf	res	1
 	code
 
 init_serial:
-#if BITBANG_ENABLED
-	bcf	bits, BIT_BITBANG_ACTIVITY
-#endif
-	
 	bsf	STATUS, RP0	; move to page 1
 
 	;; The USART requires that bits [21] of TRISB are enabled, or you'll
 	;; get unpredictable results from it. Some PICs won't work at all,
 	;; and others will look like they're working but fail unpredictably.
-	bsf	TRISB, 2
-	bsf	TRISB, 1
-	
-	;; high-speed port speed math:
-	;; Desired baud rate = Fosc / (16 * (X + 1))
-	;; e.g. 4800 = 10000000 / (16 * (X + 1))
-	;;      2083.333 = 16X + 16
-	;;      2067.333 = 16X
-	;;      X = 129.2 (use 129)
-	;; other values @ 10MHz clock:
-	;; 4800: X = 129 (129.2)
-	;; 9600: X = 64 (64.104)
-	;; 19200: X = 31.55 (works @ 32)
-	;; 38400: X = 15.27 (not usable)
-	;; 57600: X = 9.85
-	;; 115200: 4.43
-	;; 230400: 1.7127
+	bsf	USART_TX_TRIS
+	bsf	USART_RX_TRIS
 
-	;; low-speed math:
-	;; Desired baud rate = Fosc / (64 * (X + 1))
-	;; e.g. 4800 = 10000000 / (64 * (X + 1))
-	;;      2083.333 = 64X + 64
-	;;      2019.333 = 64X
-	;;      X = 31.55 (use 32)
-	;; Others @ 10MHz:
-	;;    1200: 129.2
-	;;    2400: 64.1
-	;;    4800: 31.55 (verified, 32 works)
-	;;    9600: 15.28 (use 15; not tested)
-	;;    19200: 7.14 (use 7; works)
-	;;    38400: 3.07 (tried 3; not stable)
-	;;    57600: 1.7126 (not tested, seems unlikely to work)
-	
-	bsf	TXSTA, BRGH	; high-speed mode if 'bsf'; low-speed for 'bcf'
-	movlw	d'12'		; 'X', per above comments, to set baud rate
-	movwf	SPBRG
+#if USART_HIGHSPEED
+	bsf     TXSTA, BRGH ; high-speed mode if 'bsf'; low-speed for 'bcf'
+#else
+	bcf     TXSTA, BRGH ; low-speed
+#endif
+	movlw   USART_BAUD_INITIALIZER ; constant for baud speed
 
 	bcf	TXSTA, CSRC	; unimportant
 	bcf	TXSTA, TX9	; 8-bit
@@ -118,10 +92,6 @@ init_serial:
 
 	bsf	RCSTA, SPEN	; serial port enabled
 
-	;; Short delay for startup...
-	movlw	0xFA
-	call	delay_ms
-	
 	movfw	RCREG		; flush 3-deep buffer
 	movfw	RCREG
 	movfw	RCREG
@@ -166,12 +136,6 @@ putch_block:
 ;;; * received, echo it back out the usart.
 ;;; ************************************************************************
 getch_usart:
-#if BITBANG_ENABLED
-	;; Test the bitbang interface. If the SERIALRX line is clear, then
-	;; we've got activity and should note that for future reference.
-	btfss	SERIALRX
-	bsf	bits, BIT_BITBANG_ACTIVITY
-#endif
  	btfsc	RCSTA, OERR	; check for overrun
  	goto	overrun
 
@@ -185,7 +149,7 @@ retry:
 	btfsc	RCSTA, FERR
 	goto	retry
 	
-#if ECHO
+#if USART_ECHO
 	movwf	echo_buf	; save a copy
 	call	putch_usart	; send a copy back out
 	movfw	echo_buf	; restore the saved copy
@@ -199,185 +163,109 @@ overrun	bcf	RCSTA, CREN	; Clear overrun. Documented procedure: clear
 	bsf	RCSTA, CREN
 	goto	getch_usart	; retry
 
-;;; ************************************************************************
-;;; * getch
-;;; *
-;;; * bit-banging receiver. Blocks until the char is received.
-;;; *
-;;; * Output:
-;;; *    W	byte received
-;;; *
-;;; * Destroys:
-;;; *    loopcounter, byte, i, arg1
-;;; ************************************************************************
+;;; ; ************************************************************************
+;;; ; * getch_usart_timeout
+;;; ; *
+;;; ; * Wait about a second for a character from the USART. When a char is
+;;; ; * received, echo it back out the usart. If nothing is received before
+;;; ; * the timeout, we return 0. So the caller can't expect 0 as a valid
+;;; ; * return character...
+;;; ; ************************************************************************
 
-#if BITBANG_ENABLED
+getch_usart_timeout:
+	banksel serial_timeout_0
+	clrf    serial_timeout_0
+	clrf    serial_timeout_1
+	clrf    serial_timeout_2
+	banksel 0
+getch_usart_timeout_loop:
+	btfsc   RCSTA, OERR ; check for overrun
+	goto    overrun_timeout
 
-getch:
-	goto	getch_loop_start
-
-getch_loop:
-	clrwdt
-getch_loop_start:
-	btfsc	SERIALRX
-	goto	getch_loop
-
-	nop
-	CLRF	i
-	MOVLW	0x14
-	MOVWF	loopcounter
-getch_wait1:			; initial delay; get past start bit
-	DECFSZ	loopcounter, F
-	goto	getch_wait1
-
-	movlw	0x08
-	movwf	byte
-getch_get_bit:
-	clrwdt
-	movlw	0xAA
-	movwf	loopcounter
-	
-getch_wait:
-	decfsz	loopcounter, F	; this delay is 509 cycles, ~ .00020306 seconds
-	goto	getch_wait
-	
-	bcf	STATUS, C
-	nop
-	RRF	i, F
-	btfsc	SERIALRX
-	bsf	i, 7
-	nop
-	DECFSZ	byte, F
-	goto	getch_get_bit
-
-getch_stopbit_loop:
-	nop
-	nop
-	btfss	SERIALRX
-	goto	getch_stopbit_loop
-
-gotresult:	
-	movfw	i		; move result into W
-	bcf	bits, BIT_BITBANG_ACTIVITY ; turn off activity flag
-	bcf	STATUS, C	; no error, so clear the carry and return
-	bcf	STATUS, DC	; no framing error, so clear that too
-	return
-
-#endif
-
-;;; ************************************************************************
-;;; * getch_timeout
-;;; *
-;;; * bit-banging receiver. Does NOT block; will return with carry set
-;;; * and 0 in W if nothing is received in about 6 seconds.
-;;; *
-;;; * Output:
-;;; *    W	byte received
-;;; *	 Carry set on failure
-;;; *	 DC set on framing error
-;;; *
-;;; * Destroys:
-;;; *    loopcounter, byte, i, arg1
-;;; ************************************************************************
-
-#if BITBANG_ENABLED
-
-getch_timeout:
-	bcf	bits, BIT_BITBANG_ACTIVITY ; clear the activity bit
-	
-	;; set up 3-byte timeout timer to provide about a 7-second wait.
-	clrf	timeout+2
-	clrf	timeout+1
-	movlw	0xE0
-	movwf	timeout+0
-	goto	L07E1
-
-L07E0:	clrwdt
-L07E1:	bsf	STATUS, C	; assume error condition.
-	bcf	STATUS, DC	; DC only set on framing error.
-	
-	btfss	SERIALRX
-	goto	getbits
-
-	incfsz	timeout+2, F	; incfsz does not affect any status registers
-	goto	L07E0		; (not even Z!), so the state of the STATUS
-	incfsz	timeout+1, F	; register that we set will carry through.
-	goto	L07E0
-	incfsz	timeout+0, F
-	goto	L07E0
-	retlw	0x00		; timeout (about 6.7 seconds). Carry still set.
-
-getbits:
-	nop
-	CLRF	i
-	MOVLW	0x14
-	MOVWF	loopcounter
-getch_t_wait1:			; initial delay; get past start bit
-	DECFSZ	loopcounter, F
-	goto	getch_t_wait1
-
-	;; now check to see that the start bit is still there. We
-	;; delayed about .0000252 seconds (which is about 1/10 of a bit at
-	;; 4800 baud), so we should be in the window where it's still on.
-	;; If it's not on, then we have a framing error; to keep our timer
-	;; running, we'll just jump back into the timer loop above and wait
-	;; for the start bit again.
-	btfsc	SERIALRX
-	goto	L07E0
-	
-	
-	movlw	0x08
-	movwf	byte
-	
-L07EC:
-	clrwdt
-	movlw	0xAA
-	movwf	loopcounter
-	
-getch_t_wait:
-	decfsz	loopcounter, F	; this delay is 509 cycles, ~ .00020306 seconds
-	goto	getch_t_wait
-	
-	bcf	STATUS, C	; total delay of 3 + 169 * 3 + 2 + 9 cycles
-	nop			; that's .0002084 seconds, which is damn
-	RRF	i, F		; close to 1/4800 (.000208333) between label
-	btfsc	SERIALRX	; L07EC and getch_t_get_stopbit
-	bsf	i, 7
-	nop
-	DECFSZ	byte, F
-	goto	L07EC
-
-	;; look for the stop bit. If we don't find it by the time the
-	;; stop bit should have arrived (that is, .000208333 seconds),
-	;; then it's a framing error; return 0.
-	;; The timing for this is 3 + 6 * 84 + 5 cycles. That's slightly
-	;; longer than one bit-time for 4800 baud.
-
-	;; Can't get this routine working! Going to try to remove this
-	;; stopbit test in order to see if that makes a difference. In
-	;; theory, this should be a benign change, since the only case where
-	;; we get this far is when we have something seeming to be a pulse,
-	;; followed by something looking like a start bit... (FIXME)
-
-	;; That didn't do anything. Putting this back now, along with a
-	;; loop around the start bit detection. Should time out after ~7 secs.
-
-getch_t_get_stopbit:
-	bsf	STATUS, C	; assume error condition
-	bsf	STATUS, DC	; (and it will be a framing error)
-	movlw	0x55
-	movwf	loopcounter
-getch_t_stopbit_loop:
-	nop
-	nop
-	decf	loopcounter, F
+;;;  increment timeout timer. If we roll over, we're done.
+	banksel serial_timeout_0
+	incfsz  serial_timeout_0, F
+	goto    getch_usart_timeout_loop1
+	incfsz  serial_timeout_1, F
+	goto    getch_usart_timeout_loop1
+	incf    serial_timeout_2, F
+	movfw   serial_timeout_2
+	banksel 0
+	xorlw   0x02	; somewhere around a half second @ 20MHz
 	skpnz
-	goto	L07E0		;	retlw	0x00
-	btfss	SERIALRX
-	goto	getch_t_stopbit_loop
+	retlw   0x00	; failed to rx in the allotted time
 
-	goto	gotresult	; same as the blocking version from here on
-#endif
+getch_usart_timeout_loop1:
+	banksel 0
+	btfss   PIR1, RCIF ; make sure there's data to receive
+	goto    getch_usart_timeout_loop ; loop if not
 	
-			
+	movfw   RCREG	; grab the received character
+	
+;;;  check for framing errors
+	btfsc   RCSTA, FERR
+	goto    getch_usart_timeout_loop
+
+#if USART_ECHO
+	movwf   echo_buf ; save a copy
+	call    putch_usart ; send a copy back out
+	movfw   echo_buf    ; restore the saved copy
+#endif
+	return
+overrun_timeout:
+	bcf     RCSTA, CREN ; Clear overrun. Documented procedure: clear
+	movfw   RCREG	    ; CREN, then flush the fifo by reading three
+	movfw   RCREG	    ; bytes (the size of the backing store), and
+	movfw   RCREG	    ; then re-enable CREN.
+	bsf     RCSTA, CREN
+	goto    getch_usart_timeout_loop ; retry
+	
+;;; ************************************************************************
+;;; * putch_hex_usart
+;;; *
+;;; * put the byte's value, in hex, on the serial usart.
+;;; *
+;;; * Input:
+;;; *
+;;; *   W       byte to send
+;;; *
+;;; * FIXME: assumes 'byte' is in page 0, same as SERIALTX
+;;; ************************************************************************
+
+putch_hex_usart:
+	banksel serial_work_tmp
+	movwf   serial_work_tmp
+	swapf   serial_work_tmp, W
+	banksel 0
+	andlw   0x0F	; grab low 4 bits of serial_work_tmp
+	sublw   0x09	; Is it > 9?
+	skpwgt		;   ... yes, so skip the next line
+	goto    send_under9 ; If so, go to send_under9
+	sublw   0x09	    ; undo what we did
+	addlw   'A' - 10    ; make it ascii
+	goto    send_hex
+	
+send_under9:
+	sublw   0x09	; undo what we did
+	addlw   '0'	; make it ascii
+send_hex:
+	call	putch_usart
+
+	banksel serial_work_tmp
+	movfw   serial_work_tmp
+	banksel 0
+	andlw   0x0F
+	sublw   0x09
+	skpwgt
+	goto    send_under9_2
+	sublw   0x09	; undo what we did
+	addlw   'A' - 10 ; make it ascii
+	goto    putch_usart
+
+send_under9_2:
+	sublw   0x09	; undo what we did
+	addlw   '0'	; make it ascii
+	goto    putch_usart
+	
+	
 	END
